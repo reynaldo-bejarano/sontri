7009ed14416dbd1bd53cb1d3252658ebb6d48814
unknown <reynaldo.bjsz@gmail.com>
2022-09-29 17:51:15 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const sr = ScrollReveal({
+    origin: 'top', 
+    distance: '30px',
+    duration: 2000,
+    reset: true,
+});
+sr.reveal(`#banner, #acta, #problematica, #historias, #clasificacion, #cronograma`, {
+    inteval: 200
+})
